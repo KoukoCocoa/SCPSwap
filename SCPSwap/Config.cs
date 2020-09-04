ed0a11e1db526e58a852ad221d0cddf8600d1d90
Aevann1 <59999695+Aevann1@users.noreply.github.com>
2020-09-04 08:46:52 +0200

    Update Config.cs

--- SCPSwap/Config.cs
+++ SCPSwap/Config.cs
@@ -8,22 +8,22 @@ namespace ScpSwap
     {
         public bool IsEnabled { get; set; } = true;
         public bool DisplayStartMessage { get; set; } = true;
-        public bool SwapAllowNewScps { get; set; } = false;
-        public bool SwapAllowDuplicatesOfTheSameScp { get; set; } = false;
         public float SwapTimeout { get; set; } = 60f;
         public float SwapRequestTimeout { get; set; } = 20f;
         public ushort StartMessageTime { get; set; } = 15;
-        public string DisplayMessageText { get; set; } = "<color=yellow><b>Did you know you can swap classes with other SCP's?</b></color> Simply type <color=orange>.scpswap (role number)</color> in your in-game console (not RA) to swap!";
         public List<int> SwapBlacklist { get; private set; } = new List<int>() { 10 };
+        public bool SwapAllowNewScps { get; set; } = false;
+        public bool SwapAllowDuplicatesOfTheSameScp { get; set; } = false;
         public Dictionary<int, int> DisallowedScpCompinations { get; set; } = new Dictionary<int, int>
+		{
 			{
-				{
-					0, 9
-				},
-				{
-					9, 0
-				},
-			};
-
+				0, 9
+			},
+			{
+				9, 0
+			},
+		};
+        public bool AllowScpsToChangeToOtherRoles { get; set; } = true;
+        public string DisplayMessageText { get; set; } = "<b>You can change your role through the console (~ key) by typing <color=purple>.scpswap insert_role</color> If someone with that role already exists, they'll receive a request to swap they can accept or reject. You can also use <color=purple>.scpswap list</color> to list all available roles.";
 	}
 }
